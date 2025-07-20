cf20d084c3bea2b4c1693ef010c084f319239cec
GustavoFSProg <gustavo.prog40@gmail.com>
2025-07-20 12:57:37 -0300

    nova

---
+++ dist/repositories/repository.banner.js
@@ -0,0 +1,9 @@
+import { execute } from "../database/sqlite.js";
+async function Listar() {
+  const sql = "select * from banner order by ordem";
+  const banners = await execute(sql, []);
+  return banners;
+}
+export default {
+  Listar
+};
